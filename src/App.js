2b111127ed57c898dafe308c7e73a5b5cb7cd55a
Christopher Peralta <94395580+Cperalta10@users.noreply.github.com>
2022-12-01 13:29:57 -0800

    finished displaying all data and styled the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [starWarsCharacters, setStarWarsCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setStarWarsCharacters(res.data);
+      })
+      .catch((err) => {});
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character starWarsCharacters={starWarsCharacters} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,39 @@
 // Write your Character component here
+// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharacterDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    font-size: 40px;
+    padding: 1rem;
+    border: 2px green solid;
+    margin: 1rem;
+    height: 50px;
+  }
+`;
+
+export default function Character(props) {
+  const { starWarsCharacters } = props;
+
+  return (
+    <CharacterDiv>
+      {starWarsCharacters.map((objPeople, index) => {
+        return (
+          <div key={index}>
+            <h2>{objPeople.name}</h2>
+            <h2>{objPeople["birth_year"]}</h2>
+          </div>
+        );
+      })}
+    </CharacterDiv>
+  );
+}
